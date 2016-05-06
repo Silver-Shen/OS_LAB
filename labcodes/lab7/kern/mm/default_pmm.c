966d0703484d8da122cdb404b0193c9b97f3cb1e
s0122 <shenzy13@qq.com>
2016-05-06 22:02:03 +0800

    finish lab7 report

--- labcodes/lab7/kern/mm/default_pmm.c
+++ labcodes/lab7/kern/mm/default_pmm.c
@@ -9,7 +9,7 @@
    usually split, and the remainder added to the list as another free block.
    Please see Page 196~198, Section 8.2 of Yan Wei Min's chinese book "Data Structure -- C programming language"
 */
-// LAB2 EXERCISE 1: YOUR CODE
+// LAB2 EXERCISE 1: 2013011371
 // you should rewrite functions: default_init,default_init_memmap,default_alloc_pages, default_free_pages.
 /*
  * Details of FFMA
@@ -77,7 +77,7 @@ default_init_memmap(struct Page *base, size_t n) {
     base->property = n;
     SetPageProperty(base);
     nr_free += n;
-    list_add(&free_list, &(base->page_link));
+    list_add_before(&free_list, &(base->page_link));
 }
 
 static struct Page *
@@ -96,12 +96,14 @@ default_alloc_pages(size_t n) {
         }
     }
     if (page != NULL) {
+    	list_entry_t *prev =list_prev(&(page->page_link));
         list_del(&(page->page_link));
         if (page->property > n) {
             struct Page *p = page + n;
             p->property = page->property - n;
-            list_add(&free_list, &(p->page_link));
-    }
+            list_add(prev, &(p->page_link));
+            SetPageProperty(p);
+        }
         nr_free -= n;
         ClearPageProperty(page);
     }
@@ -136,7 +138,9 @@ default_free_pages(struct Page *base, size_t n) {
         }
     }
     nr_free += n;
-    list_add(&free_list, &(base->page_link));
+    le = &free_list;
+    while ((le = list_next(le)) != &free_list && le2page(le,page_link)<base);
+    list_add_before(le, &(base->page_link));
 }
 
 static size_t
